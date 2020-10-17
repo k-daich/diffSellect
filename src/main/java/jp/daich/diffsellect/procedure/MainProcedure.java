07aa950ee79697d880ddbd40b660515e7fb77010
daich <fst-qst.w-it-h@docomo.ne.jp>
2020-10-17 09:46:33 +0100

    20201017 enable to read sql.txt and genarate diff report excel

--- src/main/java/jp/daich/diffsellect/procedure/MainProcedure.java
+++ src/main/java/jp/daich/diffsellect/procedure/MainProcedure.java
@@ -11,21 +11,26 @@ public class MainProcedure {
 
     public void execute(String tableName, String sellectResult) {
         LogUtil.debug(this.getClass(), "★★★start★★★");
+        SqlTextReadProcedure sqlReader = null;
         try {
-            new SqlTextReadProcedure("./sql.txt");
-            ExcelWriter eWriter = init(tableName);
+            sqlReader = new SqlTextReadProcedure("./test/sql.txt");
+            ExcelWriter eWriter = new ExcelWriter(tableName);
 
-            // Sellect結果をBookオブジェクトに設定していく
-            eWriter.writeSellectResult(tableName, sellectResult);
-
+            String lineStr;
+            while ((lineStr = sqlReader.readLine()) != null) {
+                // Sellect結果をBookオブジェクトに設定していく
+                eWriter.writeSellectResult(tableName, lineStr);
+            }
             // エクセルファイルに書き込む
             eWriter.flush();
+        } catch (Exception e) {
+            LogUtil.debug(this.getClass(), "Exception Message :  " + e.getMessage());
+            LogUtil.debug(this.getClass(), "Exception Message :  " + e.getStackTrace());
         } finally {
+            if (sqlReader != null) {
+                sqlReader.close();
+            }
             LogUtil.debug(this.getClass(), "★★★end★★★");
         }
-    }
-
-    private ExcelWriter init(String tableName) {
-        return new ExcelWriter(tableName);
     }
 }
--- src/main/java/jp/daich/diffsellect/procedure/sub/SqlTextReadProcedure.java
+++ src/main/java/jp/daich/diffsellect/procedure/sub/SqlTextReadProcedure.java
@@ -8,26 +8,112 @@ import java.io.IOException;
 
 public class SqlTextReadProcedure {
 
-    // 読み込むファイルパス
-    private final String filePath;
+    // 読み込みReaderオブジェクト
+    private final BufferedReader reader;
+
+    private enum SQL_TXT_LINE_TYPE {
+        // クエリの行
+        QUERY,
+        // カラム名の行
+        COLUMNS_NAME,
+        // 項目値の行
+        VALUES,
+        // その他
+        OTHER
+    }
+
+    // 現在読み込んでいるSQLテキストの行種類
+    private SQL_TXT_LINE_TYPE currentLineType = SQL_TXT_LINE_TYPE.OTHER;
 
     /**
      * Constructor
      */
     public SqlTextReadProcedure(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public void execute() {
-        // 1行ずつ読み込む
-        try (BufferedReader in = new BufferedReader(new FileReader(new File(filePath)))) {
-            String line;
-            // 読み込んだ行をシステム標準出力する
-            while ((line = in.readLine()) != null)
-                System.out.println(line);
+        try {
+            reader = new BufferedReader(new FileReader(new File(filePath)));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 1行分の文字列を返す 最終行まで到達していた場合はnullを返す
+     * 
+     * @return lineString
+     */
+    public String readLine() {
+        try {
+            // 読み込んだ行をシステム標準出力する
+            return nextTargetLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 次の読み込み対象行の文字列を返す<br>
+     * 最終行まで到達した場合はnullを返す
+     * 
+     * @return nextTargetLine
+     */
+    private String nextTargetLine() throws IOException {
+        switch (currentLineType) {
+            case QUERY:
+                // QUERYの次の行はカラム名の行であるため、currentLineTypeを変更する
+                currentLineType = SQL_TXT_LINE_TYPE.COLUMNS_NAME;
+                // 1行読み込む
+                return reader.readLine();
+            case COLUMNS_NAME:
+                // カラム名の次の行はSELECT結果（値）の行であるため、currentLineTypeを変更する
+                currentLineType = SQL_TXT_LINE_TYPE.VALUES;
+                // 1行読み込む
+                return reader.readLine();
+            case VALUES:
+                String col_line;
+                if ((col_line = reader.readLine()) != "") {
+                    // 空行（SELECT結果の全件読み込み済み）でない場合はSELECT結果1件の文字列を返す
+                    return col_line;
+                }
+                return scanQueryLine();
+            case OTHER:
+                return scanQueryLine();
+            // default:
+            // throw new RuntimeException("System Error : Unknown Enum Type");
+            // break;
+        }
+        throw new RuntimeException("System Error : Unreachable Code");
+    }
+
+    /**
+     * 次のQUERY行の文字列を返す<br>
+     * QUERY行が存在しなかった場合はnullを返す
+     * 
+     * @return QUERY行の文字列
+     */
+    private String scanQueryLine() throws IOException {
+        String line;
+        // 最終行に到達するまで1行ずつ読み込む
+        while ((line = reader.readLine()) != null) {
+            // QUERY行の場合は読み込んだ文字列を返す
+            if (line.startsWith(">SQL")) {
+                currentLineType = SQL_TXT_LINE_TYPE.QUERY;
+                return line;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * ファイルをクローズする
+     */
+    public void close() {
+        try {
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
